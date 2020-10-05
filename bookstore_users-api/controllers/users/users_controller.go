ce36ab650c6f9b7548a93850c3480cc30754ad37
nmpotential <nathan.mokonz@gmail.com>
2020-10-06 00:57:35 +0100

    starting from our domain - completed.

--- bookstore_users-api/controllers/users/users_controller.go
+++ bookstore_users-api/controllers/users/users_controller.go
@@ -1,23 +1,25 @@
 package users
 
 import (
-	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/nmpotential/design-build-rest-microservices/bookstore_users-api/domain/users"
 	"github.com/nmpotential/design-build-rest-microservices/bookstore_users-api/services"
+	"github.com/nmpotential/design-build-rest-microservices/bookstore_users-api/utils/errors"
 	"net/http"
 )
 
 func CreateUser(c *gin.Context) {
 	var user users.User
-	if err := c.ShouldBindJSON(&user); err != nil{
-		fmt.Println(err)
-		//TODO: return bad request to the caller.
+	if err := c.ShouldBindJSON(&user); err != nil {
+		restErr := errors.NewBadRequestError("invalid json body")
+		// Returns bad request to the caller.
+		c.JSON(restErr.Status, restErr)
 		return
 	}
 	result, saveErr := services.CreateUser(user)
 	if saveErr != nil {
-		//TODO: Handle user creation error
+		// Handles user creation error
+		c.JSON(saveErr.Status, saveErr)
 		return
 	}
 	c.JSON(http.StatusCreated, result)
--- bookstore_users-api/services/user_service.go
+++ bookstore_users-api/services/user_service.go
@@ -1,7 +1,18 @@
 package services
 
-import "github.com/nmpotential/design-build-rest-microservices/bookstore_users-api/domain/users"
+import (
+	"github.com/nmpotential/design-build-rest-microservices/bookstore_users-api/domain/users"
+	"github.com/nmpotential/design-build-rest-microservices/bookstore_users-api/utils/errors"
+	"net/http"
+)
 
-func CreateUser(user users.User) (*users.User, error) {
+func CreateUser(user users.User) (*users.User, *errors.RestErr) {
+	return nil, nil
+
 	return &user, nil
+
+	return &user, &errors.RestErr{
+		Status: http.StatusInternalServerError,
+	}
+
 }
